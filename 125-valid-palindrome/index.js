4f7c13e58b46c147cc52e1e4f22bbc357c681ab0
iediong <ediongi6@hotmail.com>
2022-05-25 20:13:23 +0100

    Add solution for valid palindrome I

---
+++ 125-valid-palindrome/index.js
@@ -0,0 +1,25 @@
+/**
+ * 125. Valid Palindrome (Easy)
+ * @param {string} s
+ * @return {boolean}
+ */
+
+const isPalindrome = function(s) {
+    let str = s.toLowerCase().split(' ').join('').replace(/[^a-z0-9]/g, '');
+    let start = 0; 
+    let end = str.length - 1;
+
+    while (start < end) {
+        if (str[start] !== str[end]) {
+            return false;
+        } else {
+            start++;
+            end--;
+        }
+    }
+
+    return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
+console.log(isPalindrome("0P"));
